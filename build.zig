9d02564952ad22daa837712dcac51e2a00ba11ce
mlycko <martin@mlycko.com>
2024-12-05 17:57:02 +0000

    Module added

--- build.zig
+++ build.zig
@@ -24,6 +24,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    _ = b.addModule("scramble", .{});
+
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
