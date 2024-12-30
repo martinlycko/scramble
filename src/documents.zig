15b5dc4432673dfe655681c08a53cffd06b9336d
mlycko <martin@mlycko.com>
2024-12-30 18:56:38 +0000

    Documents idea added

---
+++ src/documents.zig
@@ -0,0 +1,6 @@
+// Will create new structs for documents with attributes like author, source, title
+// Documents return a type, so that individual fields can be added to capture structured info (e.g. a salary field for a job description)
+// Documents can be created from files (and create all in a directory)
+// Some functioanlity to ensure not duplicate documents are created?
+// Documents will own all their content, so other analyses can slices instead of duplicating strings
+// That will mean to rewrite some of the content analysis section
