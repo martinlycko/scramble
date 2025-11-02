978fa823486f6ee7969b6f70df9dec2d10b417bf
mlycko <martin@mlycko.com>
2025-11-02 17:36:41 +0000

    Documents page with document list

---
+++ src/lib/project.svelte.js
@@ -0,0 +1,4 @@
+export const project = $state({name: "My project",
+    file: "path",
+    documents: [{id: 1, title: "Document 1", text: "test1"}, {id: 2, title: "Document 2", text: "test2"}],
+})
---
+++ src/routes/documents/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { project } from '$lib/project.svelte.js';
+
+  let { children } = $props();
+</script>
+
+<div>
+  <div class="left-column">
+    <div class="row">
+        <h3 class="row-item">Documents</h3>
+        <div class="row-item right-button">
+            <a href='/documents/add'>+</a>
+        </div>
+    </div>
+    <div class="scrollable">
+        <ul class="ColumnList">
+            {#each project.documents as document}
+                <li class="ColumnListItem"><a href="/documents/{document.id}">{document.title}</a></li>
+            {/each}
+        </ul>
+    </div>        
+  </div>
+</div>
+
+{@render children?.()}
+
+<style>
+  .left-column{
+        position: fixed;
+        top: 0px;
+        left: 50px;
+        float: left;
+        height: 100%;
+        width: calc(20% - 50px);
+        background-color:#aaa;
+    }
+
+    .row {
+        display:table;
+        background-color:#981111;
+        width: 100%;
+    }
+
+    .row-item {
+        display:table-cell;
+    }
+
+    .right-button {
+        text-align: right;
+    }
+
+    .ColumnList {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        padding: 0px;
+    }
+
+    .ColumnListItem {
+        color: black;
+        text-align: left;
+        text-decoration: none;
+    }
+
+    .scrollable {
+        height: calc(100% - 40px);
+        overflow-y: auto;
+    }
+</style>
